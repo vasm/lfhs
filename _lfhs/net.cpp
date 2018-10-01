0abc42271c3a6bb2a5a312d76ca188aa31ff41b5
Vasiliy Sabadazh <vsabadazh@gmail.com>
2018-10-01 18:06:17 +0300

    added cpp version (qmake)

---
+++ _lfhs/net.cpp
@@ -0,0 +1,11 @@
+#include "net.h"
+
+
+//bool is_bad_socket(socket_t s)
+//{
+//#ifdef __unix__
+//    return s > 0;
+//#elif defined(_MSC_VER)
+//    return s != ~0ul/*INVALID_SOCKET*/;
+//#endif
+//}
---
+++ _lfhs/net.h
@@ -0,0 +1,41 @@
+#ifndef NET_H
+#define NET_H
+
+/*
+ * Platform-dependent includes
+ */
+
+
+#ifdef __unix__ // we're good
+    #define POSIX
+    #include <sys/ioctl.h>
+    #include <sys/socket.h>
+    #include <sys/time.h>
+    #include <netinet/in.h>
+
+    typedef int socket_t;
+    typedef int ioctlarg_t;
+
+    inline bool is_bad_socket(socket_t socket_handle) { return socket_handle < 0; }
+
+
+#elif defined(_MSC_VER)
+    #include <WinSock2.h>
+    //#include <ws2tcpip.h>
+    #include <stdio.h>
+    #pragma comment(lib, "Ws2_32.lib")
+
+    typedef SOCKET socket_t;
+    typedef u_long ioctlarg_t;
+
+    int close(socket_t s) { return closesocket(s); }
+    int ioctl(socket_t s, long cmd, u_long* argp) { return ioctlsocket(s, cmd, argp); }
+
+    inline bool is_bad_socket(socket_t socket_handle) { return socket_handle == ~0ul; }
+#elif true
+    #error "Compiling on unknown platform"
+#endif
+
+
+
+#endif // NET_H
